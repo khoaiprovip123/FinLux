d594f1151ea4d1d23a6d2c576830619326d06028
FinLux Developer <developer@finlux.app>
2026-08-12 11:49:05 +0700

    chore: ignore .kotlin and update navigation

--- app/src/main/java/com/finlux/app/core/navigation/FinluxNavHost.kt
+++ app/src/main/java/com/finlux/app/core/navigation/FinluxNavHost.kt
@@ -30,7 +30,6 @@ import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.graphicsLayer
 import androidx.compose.ui.input.pointer.pointerInput
 import androidx.compose.ui.input.pointer.PointerEventPass
-import androidx.compose.ui.input.pointer.awaitPointerEventScope
 import androidx.compose.ui.platform.LocalDensity
 import androidx.compose.ui.unit.dp
 import androidx.navigation.NavHostController
